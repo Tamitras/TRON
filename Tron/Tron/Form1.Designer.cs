7110b3b08738964f156f16b034fca059d7889dac
Erik <erik.kaufmann@eder.de>
2019-02-15 14:02:07 +0100

    wrote first code - yeah!

--- Tron/Tron/Form1.Designer.cs
+++ Tron/Tron/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxProcess = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCurrentProcess = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStartConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxProcess
+            // 
+            this.listBoxProcess.FormattingEnabled = true;
+            this.listBoxProcess.Location = new System.Drawing.Point(12, 34);
+            this.listBoxProcess.Name = "listBoxProcess";
+            this.listBoxProcess.Size = new System.Drawing.Size(186, 394);
+            this.listBoxProcess.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Alle Prozesse";
+            // 
+            // textBoxCurrentProcess
+            // 
+            this.textBoxCurrentProcess.Location = new System.Drawing.Point(204, 34);
+            this.textBoxCurrentProcess.Multiline = true;
+            this.textBoxCurrentProcess.Name = "textBoxCurrentProcess";
+            this.textBoxCurrentProcess.Size = new System.Drawing.Size(355, 394);
+            this.textBoxCurrentProcess.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ausgewählter Prozess";
+            // 
+            // btnStartConnect
+            // 
+            this.btnStartConnect.Location = new System.Drawing.Point(565, 34);
+            this.btnStartConnect.Name = "btnStartConnect";
+            this.btnStartConnect.Size = new System.Drawing.Size(230, 23);
+            this.btnStartConnect.TabIndex = 4;
+            this.btnStartConnect.Text = "Connect To Process";
+            this.btnStartConnect.UseVisualStyleBackColor = true;
+            this.btnStartConnect.Click += new System.EventHandler(this.btnStartConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStartConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCurrentProcess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxProcess);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCurrentProcess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStartConnect;
     }
 }
 
--- Tron/Tron/Form1.cs
+++ Tron/Tron/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,25 @@ namespace Tron
 {
     public partial class Form1 : Form
     {
+
+        public List<Process> Processes { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+
+            Processes = new List<Process>();
+        }
+
+        public void Initialize()
+        {
+            // Filtern nach name (sortby c=> c.name) etc.
+            Processes = Process.GetProcesses().ToList();
+        }
+
+        private void btnStartConnect_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
